3911780c61d30feda6ad9eba8b95fa0de67bcd67
sofflen <nixxesgaming@gmail.com>
2024-09-16 22:02:01 +0300

    initial commit

---
+++ src/main/java/com/study/spring6ai/Spring6AiApplication.java
@@ -0,0 +1,13 @@
+package com.study.spring6ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6AiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring6AiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/study/spring6ai/Spring6AiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.spring6ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6AiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
